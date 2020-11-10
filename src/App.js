a88af7102253baade3ba038ad313269cfe5f362f
Mo <olfatnia.m@gmail.com>
2020-11-09 17:13:31 -0800

    added react-router-dom and connected hatpages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      BBC
+    </h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
