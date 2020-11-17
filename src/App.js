f0103996e64d22f7c19e393fd8c3b39fbb327b13
Mo <olfatnia.m@gmail.com>
2020-11-16 19:57:36 -0800

    header_ns

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import {Route, Switch} from 'react-router-dom';
+import Header from './components/header/header.component'
+import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>
-      BBC
-    </h1>
-  </div>
-);
 
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
